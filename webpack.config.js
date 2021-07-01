ca90aaff8f1d4d749d31ee23d028fb73f5177c89
zhi <1106010597@qq.com>
2021-07-01 08:35:50 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,24 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var path = require("path");
+
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "勇敢猪猪，不怕困难！",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
